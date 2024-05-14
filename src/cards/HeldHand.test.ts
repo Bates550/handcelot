4b6cfe08923b58ad2736b1b9035e31d0f623b33e
Sean Spearman <smspearmandev@gmail.com>
2024-05-13 22:06:19 -0700

    Detect straighth flushes

--- src/cards/HeldHand.test.ts
+++ src/cards/HeldHand.test.ts
@@ -4,7 +4,7 @@ import { HeldHand, POKER_HAND_NAMES } from "./HeldHand";
 describe("HeldHand", () => {
   describe("availableHands", () => {
     it("detects royal flushes", () => {
-      const royalFlush = [
+      const cards = [
         C.TEN_OF_SPADES,
         C.JACK_OF_SPADES,
         C.QUEEN_OF_SPADES,
@@ -12,12 +12,31 @@ describe("HeldHand", () => {
         C.ACE_OF_SPADES,
       ];
 
-      const hand = new HeldHand({ cards: royalFlush });
+      const hand = new HeldHand({ cards });
 
       expect(hand.availableHands()).toEqual([
         {
           name: POKER_HAND_NAMES.ROYAL_FLUSH,
-          cards: royalFlush,
+          cards,
+        },
+      ]);
+    });
+
+    it("detects straight flushes", () => {
+      const cards = [
+        C.NINE_OF_SPADES,
+        C.TEN_OF_SPADES,
+        C.JACK_OF_SPADES,
+        C.QUEEN_OF_SPADES,
+        C.KING_OF_SPADES,
+      ];
+
+      const hand = new HeldHand({ cards });
+
+      expect(hand.availableHands()).toEqual([
+        {
+          name: POKER_HAND_NAMES.STRAIGHT_FLUSH,
+          cards,
         },
       ]);
     });
--- src/cards/HeldHand.ts
+++ src/cards/HeldHand.ts
@@ -1,5 +1,41 @@
 import { Card } from "./Card";
 
+const isRoyal = (cards: Card[]): boolean => {
+  const ranks = cards.map((card) => card.rank);
+  const hasStraightRoyals =
+    ranks.includes(1) && // A
+    ranks.includes(13) && // K
+    ranks.includes(12) && // Q
+    ranks.includes(11) && // J
+    ranks.includes(10);
+  return hasStraightRoyals;
+};
+
+const isStraight = (cards: Card[]) => {
+  for (let i = 0; i < cards.length - 1; ++i) {
+    const rank = cards[i].rank;
+    const nextRank = cards[i + 1].rank;
+
+    if (rank + 1 !== nextRank) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const isFlush = (cards: Card[]) => {
+  for (let i = 0; i < cards.length - 1; ++i) {
+    const suit = cards[i].suit;
+    const nextSuit = cards[i + 1].suit;
+
+    if (suit !== nextSuit) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 export const POKER_HAND_NAMES = {
   ROYAL_FLUSH: "royalFlush",
   STRAIGHT_FLUSH: "straightFlush",
@@ -28,11 +64,24 @@ export class HeldHand {
   }
 
   availableHands(): PokerHand[] {
-    return [
-      {
-        name: POKER_HAND_NAMES.ROYAL_FLUSH,
-        cards: this.cards,
-      },
-    ];
+    this.cards.sort((a, b) => {
+      return a.rank - b.rank;
+    });
+    const availableHands: PokerHand[] = [];
+    if (isFlush(this.cards)) {
+      if (isRoyal(this.cards)) {
+        availableHands.push({
+          name: POKER_HAND_NAMES.ROYAL_FLUSH,
+          cards: this.cards,
+        });
+      } else if (isStraight(this.cards)) {
+        availableHands.push({
+          name: POKER_HAND_NAMES.STRAIGHT_FLUSH,
+          cards: this.cards,
+        });
+      }
+    }
+
+    return availableHands;
   }
 }
